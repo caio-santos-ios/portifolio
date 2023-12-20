64ec5d6791ea297056b75cee533c7cf8397f8d8d
caiosantos.dev@outlook.com <seu@email.com>
2023-12-20 16:37:46 -0300

    finalizando projeto com loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -8,7 +8,9 @@ const loading = () => {
         <main className="body bg-[var(--bg-body)] text-[var(--color-body)]">
             <section className="section h-[87vh] flex flex-col items-center gap-2">
                 <Image className="w-96" width={100} height={100} alt="imagem de loading" src={photoLoading} />
-                <h1 className="text-center">Carregando...</h1>
+                <div className="flex items-center justify-center h-screen">
+                    <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-[var(--color-body)] border-solid"></div>
+                </div>
             </section>
         </main>
     )
