a20749455b61e71227af99c378a2ad9e85378608
Caio Santos <122380293+caio-santos-ios@users.noreply.github.com>
2026-03-16 15:30:47 -0300

    Implement sitemap function for metadata route

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,1 +1,19 @@
-sitemap.ts
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://caiodesenvolvedor.com.br',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    // Se você tiver outras páginas, adicione-as aqui:
+    // {
+    //   url: 'https://caiodesenvolvedor.com.br/projetos',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.8,
+    // },
+  ]
+}
